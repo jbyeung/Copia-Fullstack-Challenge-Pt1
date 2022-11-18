90c40ca462deed955a03a66de59e18fd437845a6
jbyeung <macbook@macbook-air.lan>
2022-11-18 11:06:36 -0500

    added e164 reformat for phone numbers

--- index.py
+++ index.py
@@ -2,11 +2,13 @@ from flask import Flask
 import csv
 import json
 import time
+import phonenumbers
 
 app = Flask(__name__)
 
 @app.route("/", methods=['GET'])
 def getJSON():
+#GET endpoint, returns parsed CSV as JSON
 
 	startTime = time.time()
 
@@ -16,14 +18,25 @@ def getJSON():
 
 		for row in csvReader:
 			key = row['id']
+			row['phone'] = reformat_e164(row['phone'], row['country'])
 			table[key] = row
 
 		jsonDict = json.dumps(table)
 
 
-	print(time.time() - startTime)	#2.8 milliseconds
+	print(time.time() - startTime)	# ~3 milliseconds
 
 	return jsonDict
+
+
+
+#--------------------------------------------------------------------------------
+def reformat_e164(phone, country):
+#reformats phone number 123-456-7890 into e164 format, +X1234567890
+
+	country_code = phonenumbers.country_code_for_region(country)
+	return "+" + str(country_code) + phone.replace('-', '')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
